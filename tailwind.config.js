0cd2557607862d738e959d14fbba987df3037a60
ElvisCA01 <mark.nicols@hotmail.com>
2022-10-19 22:41:50 -0500

    Flowbite added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  plugins: [
+    require('flowbite/plugin')
   ],
   theme: {
     extend: {},
